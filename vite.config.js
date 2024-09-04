fd0d9d99082e6a612b434482e6d7cf572440a26f
unknown <thealpha9403@gmail.com>
2024-09-03 22:48:44 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-practice/', // Configura la base para el despliegue en GitHub Pages
+  base: '/Tic-Tac-Toe/', // Configura la base para el despliegue en GitHub Pages
 })
