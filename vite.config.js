68ee728d3da80cc8811b2ecf50cd89e94b0a67a6
unknown <thealpha9403@gmail.com>
2024-09-04 00:18:19 -0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Tic-Tac-Toe/',  // Ajusta esto según el nombre del repositorio
-})
